bcba444039f15c3fab05eaeaed64c080ac3eef3d
Ivan Demidov <Scrum@list.ru>
2019-04-15 10:34:42 +0300

    perf: brought constants to a separate entity

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export default {
+  type: {
+    DIGIT: 9
+  },
+  errors: {
+    GAPS_MISSING: 'A gaps option must be provided',
+    GAPS_NOT_ARRAY: 'A gaps option must be Array',
+    GAPS_NOT_EMPTY: 'A gaps option must not be empty',
+    LENGTHS_MISSING: 'A lengths option must be provided',
+    LENGTHS_NOT_ARRAY: 'A lengths option must be Array',
+    LENGTHS_NOT_EMPTY: 'A lengths option must not be empty'
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-export const constants = {
-  type: {
-    DIGIT: 9
-  },
-  errors: {
-    GAPS_MISSING: 'A gaps option must be provided',
-    GAPS_NOT_ARRAY: 'A gaps option must be Array',
-    GAPS_NOT_EMPTY: 'A gaps option must not be empty',
-    LENGTHS_MISSING: 'A lengths option must be provided',
-    LENGTHS_NOT_ARRAY: 'A lengths option must be Array',
-    LENGTHS_NOT_EMPTY: 'A lengths option must not be empty'
-  }
-};
+import constants from './constants';
 
 export default (gaps, lengths, type = 'DIGIT') => {
   if (gaps === undefined) {
