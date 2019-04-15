319216e43ce141737a209cf8b559afd900036727
Ivan Demidov <Scrum@list.ru>
2019-04-15 10:36:40 +0300

    build: add buldins and file for docs

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import readPkg from 'read-pkg'
 import camelCase from 'camelcase';
 import babel from 'rollup-plugin-babel';
 import { terser } from "rollup-plugin-terser";
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
 
 const {name} = readPkg.sync()
 
@@ -9,9 +12,13 @@ export default {
     input: 'src/index.js',
     output: [
         { file: 'lib/index.js', format: 'cjs' },
-        { file: `lib/${name}.js`, format: 'iife', name: `${camelCase(name)}` }
+        { file: `lib/${name}.js`, format: 'iife', name: `${camelCase(name)}` },
+        { file: `docs/${name}.js`, format: 'iife', name: `${camelCase(name)}` }
     ],
     plugins: [
+        resolve(),
+        commonjs(),
+        builtins(),
         babel({
             plugins: [],
             presets: [
